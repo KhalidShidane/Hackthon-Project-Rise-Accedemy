df4b3d77fbdfadf5a3373b466bb3694bfa31f11f
khalidshidane <khalidshidane@gmail.com>
2026-08-24 10:29:39 +0300

    Middleware Kusoo daray

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,13 @@
-const express = require("express");
+const mongoose =require('mongoose')
+
+const express = require('express')
+
+const App = express();
+
+mongoose.connect("mongodb://localhost:27017/Hackthon").then(()=>{
+    console.log("Data base Connected")
+})
+
+App.listen(5000,()=>{
+    console.log("Server is Runing")
+})
