eb518961758ebf1525894286a24d889816c99397
khalidshidane <khalidshidane@gmail.com>
2026-08-24 11:04:53 +0300

    waxaan ku daray User Router

--- Backend/Router/UserRouter.js
+++ Backend/Router/UserRouter.js
@@ -1,21 +1,28 @@
-const express = require('express')
+const express = require("express");
 
 const Router = express.Router();
 
 const UserController = require("../Controller/UserController");
 
-const upload = require("../middleware/upload")
-
-Router.post("/", uploadimage.single("image"),UserController.create)
-
-Router.get("/",UserController.read )
-
-Router.delete("/:id",UserController.deleteimage)
-
-Router.get("/:id",UserController.getsingle)
-
-Router.put("/:id", uploadimage.single("image"),UserController.update)
+const upload = require("../middleware/upload");
 
 
+Router.post("/",upload.single("image"),UserController.create
+);
 
-module.exports=  Router
+
+Router.get( "/", UserController.read
+);
+
+
+Router.get( "/:id",UserController.getsingle
+);
+
+Router.put( "/:id",upload.single("image"), UserController.update
+);
+
+
+Router.delete( "/:id", UserController.deleteUser
+);
+
+module.exports = Router;
