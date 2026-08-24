f01718f656063d7190e28eefe774075f6af9e068
elifeyy12 <elifmohamedsalah@gmail.com>
2026-08-24 10:36:31 +0300

    index kuso dare

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+
+app.use(express.json());
+
+mongoose.connect("mongodb://localhost:27017/Hackthone").then(() => {
+    console.log("connected to database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+
+  
+
+  
+app.listen(5000, () => {
+  console.log("server is running on port 5000");
+});
