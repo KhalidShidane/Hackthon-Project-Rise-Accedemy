4ec9126036f7dcab7031e438a26b9060947c2675
khalidshidane <khalidshidane@gmail.com>
2026-08-24 12:04:42 +0300

    user 1 abuuray

--- Backend/Model/Project.js
+++ Backend/Model/Project.js
@@ -33,7 +33,11 @@ const ProjectSchema = new mongoose.Schema({
   },
   deadline : {
     type: Number,
+<<<<<<< HEAD
     required : true,
+=======
+    required : true
+>>>>>>> 8eb7b3b (waxaan kusoo Daray user kii 1aad)
   },
 
 
@@ -45,4 +49,9 @@ const ProjectSchema = new mongoose.Schema({
   
 });
 
+<<<<<<< HEAD
 module.exports = mongoose.model("Project", ProjectSchema)
+=======
+module.exports = mongoose.model("Project", ProjectSchema);
+
+>>>>>>> 8eb7b3b (waxaan kusoo Daray user kii 1aad)
--- Backend/index.js
+++ Backend/index.js
@@ -5,25 +5,32 @@ const ProjectRouter= require("./Router/ProjectRouter");
 
 const app = express();
 
+app.use(express.json());
+
 
 app.use(express.json());
 
 mongoose.connect("mongodb://localhost:27017/Hackthone").then(() => {
     console.log("connected to database");
-  })
-  .catch((err) => {
+  })  .catch((err) => {
     console.log(err);
   });
-
-
 
   
   const Router = require("./Router/UserRouter")
   const Roouter = require("./Router/ProjectRouter")
 
+<<<<<<< HEAD
   app.use("/user", Router)
   app.use ("/project",Roouter)
   
+=======
+  const Router = require("./Router/UserRouter")
+
+  app.use("/user", Router)
+
+
+>>>>>>> 8eb7b3b (waxaan kusoo Daray user kii 1aad)
 app.listen(5000, () => {
   console.log("server is running on port 5000");
 });
