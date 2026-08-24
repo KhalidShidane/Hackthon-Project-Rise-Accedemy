726e01d907481d6ea5f2d74f630c104b2e5e44c2
muscabmacalinabdi-beep <muscabmacalinabdi@gmail.com>
2026-08-24 11:16:56 +0300

    this if footer and home page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,19 @@
-import Home from "./assets/PAGES/Home";
-import About from "./assets/PAGES/About";
-import {Routes, Route} from "react-router-dom";
-import Header from "./assets/components/Header";
-function App(){
-    return(
+import Home from "./PAGES/Home";
+import About from "./PAGES/About";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+function App() {
+    return (
         <>
-<Header/>
+            <Header />
 
-<Routes>
+            <Routes>
 
-    <Route  path="/" element={ <Home/>} />
-    <Route  path="/about" element={ <About/>} />
-</Routes>
-        
-       </>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+
+        </>
     )
 }
 export default App;
--- Frontend/src/assets/PAGES/Home.jsx
+++
@@ -1,9 +0,0 @@
-
-function Home() {
-    return(
-        <>
-        <h1>Welcome to our company</h1>
-        </>
-    )
-}
-export default Home;
