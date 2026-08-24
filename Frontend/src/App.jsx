92fd67f4f873fb393463f25ff074273e77989bad
فهيمة ضول❤️🫶 <niitapeaabdulahi@gmail.com>
2026-08-24 14:39:42 +0300

    Signup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,18 +3,16 @@ import About from "./PAGES/About";
 import Contact from "./PAGES/Contact";
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-<<<<<<< HEAD
-<<<<<<< HEAD
+
+
 import Footer from "./components/Footer";
-=======
-import Gigs from "./Dashbod/Gigs";
->>>>>>> 4ddcedf08a092e4b92e2a221e9e49cedf1ab9fb6
-=======
+
+
 import { ProtectedRoute, PublicOnlyRoute } from "./components/RouteGuards";
 import Gigs from "./Dashbod/Gigs";
 import Login from "./PAGES/Login";
 import Signup from "./PAGES/Signup";
->>>>>>> c763b0b (Wxaaan kusoo Daray Login+Sing Up)
+
 function App() {
     return (
         <>
